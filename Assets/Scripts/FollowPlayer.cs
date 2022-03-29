9fa21276bc30c12a224d3c037e5b3bda1b0d4a78
calvinbl <cb01751@srcs.org>
2022-03-29 14:51:49 -0700

    1.3
    complete

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -11,10 +11,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 20;
-    void Update()
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    void LateUpdate()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
